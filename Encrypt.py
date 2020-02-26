657bb074c62e83e44644316a2fdef94b0db04b00
atphinn <atphinn@gmail.com>
2020-02-26 15:30:59 -0500

    added Ch7 programs Encrypt and Function, added inputs to the encrypt function

---
+++ Encrypt.py
@@ -0,0 +1,42 @@
+#Transposition Cipher Encryption
+#https://nostarch.com/crackingcodes (BSD Licensed)
+
+import pyperclip
+
+def main():
+    myMessage = input('Enter a message: ')#Common sense is not so common.'
+    myKey = input('Enter a key: ') #8
+
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    #Print the encrypted string in ciphertext to the screen with a  ("pipe" character) after it in case there are spaces at the end of the encrypted message:
+    print(ciphertext + '|')
+
+    #Copy the encrypted string in ciphertet to the clipboard:
+    pyperclip.copy(ciphertext)
+
+def encryptMessage(key, message):
+    #Each string in ciphertext represent a column in the grid
+
+    ciphertext = [''] * key
+
+    #loop through each column in ciphertext:
+
+    for column in range(key):
+        currentIndex = column
+
+        #Keep looping until currentIndex goes pass the message length
+        while currentIndex < len(message):
+            #Place the character at currentIndex in message at the end of the current column in the cuphertext
+            ciphertext[column] += message[currentIndex]
+
+            #move current index over
+            currentIndex += key
+
+        #convert the ciphertext list into a single string value and return it
+    return ''.join(ciphertext)
+
+#If transpositionEncrypt.py in run (instead of inport module) cal the main() function
+
+if __name__ == '__main__':
+    main()
---
+++ Function.py
@@ -0,0 +1,9 @@
+
+def hello(name):
+    print('Hello, ' + name)
+
+print('Start')
+print('Alice')
+print('Call the function again:')
+hello('Bob')
+print ('Done')
